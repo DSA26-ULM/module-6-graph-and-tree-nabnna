a61628bede248aa293dad1b4f4cc0d0c641045db
nabnna <nabnna837@gmail.com>
2026-06-18 15:47:27 +0800

    Add files via upload

---
+++ soal3.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+struct State {
+    int r, c, dist;
+};
+
+int main() {
+    int R, C;
+    if (!(cin >> R >> C)) return 0;
+
+    vector<vector<int>> grid(R, vector<int>(C));
+    for (int i = 0; i < R; i++) {
+        for (int j = 0; j < C; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    int SR, SC, FR, FC;
+    cin >> SR >> SC >> FR >> FC;
+
+    queue<State> q;
+    // Pengganti RBT: Array 2D boolean untuk status "Visited"
+    vector<vector<bool>> visited(R, vector<bool>(C, false));
+
+    int dr[] = {-1, 1, 0, 0};
+    int dc[] = {0, 0, -1, 1};
+
+    q.push({SR, SC, 0});
+    visited[SR][SC] = true;
+
+    int shortestPath = -1;
+
+    while (!q.empty()) {
+        State curr = q.front();
+        q.pop();
+
+        if (curr.r == FR && curr.c == FC) {
+            shortestPath = curr.dist;
+            break;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            int nr = curr.r + dr[i];
+            int nc = curr.c + dc[i];
+
+            if (nr >= 0 && nr < R && nc >= 0 && nc < C) {
+                // Mengecek apakah jalan valid (0) dan belum pernah dikunjungi
+                if (grid[nr][nc] == 0 && !visited[nr][nc]) {
+                    visited[nr][nc] = true; // Tandai sudah dikunjungi
+                    q.push({nr, nc, curr.dist + 1});
+                }
+            }
+        }
+    }
+
+    cout << shortestPath << "\n";
+
+    return 0;
+}
